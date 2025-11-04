aaba4d6d4b3e8a6d9090a9f456886bd4fc31496c
Rachel <rjloh@mit.edu>
2025-11-04 16:54:49 -0500

    buggy lights

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PerspectiveTransform } from "react-perspective-transform";
 import { lightKey } from "./keyActions/lightKey";
-import { unlightKey } from "./keyActions/unlightKey";
 import { useFutureNotes } from "./FutureNotesProvider/useFutureNotes";
-import { TIME_THRESH, getAlpha } from "./constants";
+import { TIME_THRESH, getAlpha, FUTURE_NOTES_THRESH } from "./constants";
 
 export default function App() {
   const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
   const { qRef, curTimeRef } = useFutureNotes();
-  // const activeNotesRef = useRef([]);
+  const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
     let animId;
@@ -17,6 +17,7 @@ export default function App() {
     canvas.width = 400;
     canvas.height = 300;
     const ctx = canvas.getContext("2d");
+    ctxRef.current = ctx;
     const animate = () => {
       // light keys
       if (!ctx || !canvas) {
@@ -29,7 +30,6 @@ export default function App() {
         if (
           token.time - TIME_THRESH <= curTimeRef.current &&
           curTimeRef.current < token.time
-          // curTimeRef.current <= token.time + token.duration
         ) {
           lightKey(
             canvasRef.current,
@@ -44,13 +44,13 @@ export default function App() {
         }
       });
 
-      // activeNotesRef.current.forEach(({ midi, color }) => {
-      //   lightKey(canvasRef.current, midi, color);
-      // });
-
       qRef.current = qRef.current.filter(
         (token) => curTimeRef.current < token.time + token.duration
       );
+
+      if (qRef.current.length > FUTURE_NOTES_THRESH) {
+        qRef.current.length = 0;
+      }
 
       animId = requestAnimationFrame(animate);
     };
@@ -59,48 +59,47 @@ export default function App() {
     //eslint-disable-next-line
   }, []);
 
-  // const handleMIDIMessage = (color) => (event) => {
-  //   const [status, midi, velocity] = event.data;
-  //   if (status === 144 && velocity > 0) {
-  //     activeNotesRef.current.push({ midi, color });
-  //     // lightKey(canvasRef.current, midi, color);
-  //   } else if ((status == 144 && velocity == 0) || status == 128) {
-  //     activeNotesRef.current = activeNotesRef.current.filter(
-  //       (note) => note.midi !== midi
-  //     );
-  //   }
-  // };
+  const handleMIDIMessage = () => (event) => {
+    const [status, midi, velocity] = event.data;
+    if (status === 144 && velocity > 0 && midi === 36) {
+      // clear queue when pedal is pressed
+      if (!isPaused) {
+        qRef.current.clear();
+        ctxRef.current.clearRect(
+          0,
+          0,
+          canvasRef.current.width,
+          canvasRef.current.height
+        );
+        setIsPaused(true);
+      } else {
+        setIsPaused(false);
+      }
+    }
+  };
 
-  // useEffect(() => {
-  //   let midiAccess;
+  useEffect(() => {
+    let midiAccess;
 
-  //   navigator
-  //     .requestMIDIAccess()
-  //     .then((access) => {
-  //       midiAccess = access;
-  //       for (const input of midiAccess.inputs.values()) {
-  //         if (input.name === "GarageBand Virtual Out") {
-  //           continue;
-  //         }
-  //         const color =
-  //           input.name === "Piaggero"
-  //             ? "rgba(0, 131, 225, 1)"
-  //             : "rgba(239, 17, 17, 1)";
-  //         // human played notes are purple, jambot notes are red
-  //         input.onmidimessage = handleMIDIMessage(color);
-  //       }
-  //     })
-  //     .catch((err) => console.error("MIDI not supported", err));
+    navigator
+      .requestMIDIAccess()
+      .then((access) => {
+        midiAccess = access;
+        for (const input of midiAccess.inputs.values()) {
+          input.onmidimessage = handleMIDIMessage();
+        }
+      })
+      .catch((err) => console.error("MIDI not supported", err));
 
-  //   return () => {
-  //     // Cleanup MIDI listeners
-  //     if (midiAccess) {
-  //       for (const input of midiAccess.inputs.values()) {
-  //         input.onmidimessage = null;
-  //       }
-  //     }
-  //   };
-  // }, []);
+    return () => {
+      // Cleanup MIDI listeners
+      if (midiAccess) {
+        for (const input of midiAccess.inputs.values()) {
+          input.onmidimessage = null;
+        }
+      }
+    };
+  }, []);
 
   return (
     <div style={{ width: 400, height: 300, border: "1px solid #ccc" }}>
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,5 @@
 export const TIME_THRESH = 100;
+export const FUTURE_NOTES_THRESH = 500;
 
 export function getAlpha(curTime, startTime) {
   return (1.0 / TIME_THRESH) * curTime + (1.0 - startTime / TIME_THRESH); // linear alpha
