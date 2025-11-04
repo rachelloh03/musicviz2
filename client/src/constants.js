1315dbe49de796978c67f43ad2a2de4df8047b5b
Rachel <rjloh@mit.edu>
2025-11-04 16:54:56 -0500

    Revert "buggy lights"
    This reverts commit aaba4d6d4b3e8a6d9090a9f456886bd4fc31496c.

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,4 @@
 export const TIME_THRESH = 100;
-export const FUTURE_NOTES_THRESH = 500;
 
 export function getAlpha(curTime, startTime) {
   return (1.0 / TIME_THRESH) * curTime + (1.0 - startTime / TIME_THRESH); // linear alpha
